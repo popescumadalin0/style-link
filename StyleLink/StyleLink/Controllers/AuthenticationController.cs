0d3ab4c8a0b49ac7cfcb7b3a0185def24aa479b2
DizzYForester <dizzyforester@gmail.com>
2024-03-05 21:40:17 +0200

    rename project

---
+++ StyleLink/StyleLink/Controllers/AuthenticationController.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using StyleLink.Models;
+
+namespace StyleLink.Controllers;
+
+public class AuthenticationController : Controller
+{
+    private readonly ILogger<AuthenticationController> _logger;
+
+    public AuthenticationController(ILogger<AuthenticationController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ StyleLink/StyleLink/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StyleLink.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
