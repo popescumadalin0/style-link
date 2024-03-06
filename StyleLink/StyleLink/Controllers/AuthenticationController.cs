690bd5f9c94d336f29603b7c45d1d730fef7e186
DizzYForester <dizzyforester@gmail.com>
2024-03-06 20:10:28 +0200

    refactor project

--- StyleLink/StyleLink/Controllers/AuthenticationController.cs
+++ StyleLink/StyleLink/Controllers/AuthenticationController.cs
@@ -17,11 +17,4 @@ public class AuthenticationController : Controller
     {
         return View();
     }
-
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
 }
---
+++ StyleLink/StyleLink/Controllers/ErrorController.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using StyleLink.Models;
+
+namespace StyleLink.Controllers;
+
+public class ErrorController : Controller
+{
+    private readonly ILogger<ErrorController> _logger;
+
+    public ErrorController(ILogger<ErrorController> logger)
+    {
+        _logger = logger;
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
