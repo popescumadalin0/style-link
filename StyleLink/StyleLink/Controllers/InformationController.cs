24e5894d5bf8cdb34d6a6c51f1de39a5eba9da8e
DizzYForester <dizzyforester@gmail.com>
2024-03-11 23:59:44 +0200

    Implement footer

---
+++ StyleLink/StyleLink/Controllers/InformationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace StyleLink.Controllers;
+
+public class InformationController : Controller
+{
+    private readonly ILogger<InformationController> _logger;
+
+    public InformationController(ILogger<InformationController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult TermsAndConditions()
+    {
+        return View();
+    }
+
+    public IActionResult AboutUs()
+    {
+        return View();
+    }
+}
